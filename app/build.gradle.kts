d1c053cde97d2e0f71812f5ac50f441b8cfedad7
Emily Parham <eparham207@gmail.com>
2024-04-14 22:39:49 -0500

    nav commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.6.1")
     implementation ("androidx.room:room-ktx:2.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.12")
     testImplementation("junit:junit:4.12")
     debugImplementation ("androidx.fragment:fragment-testing:1.4.1")
